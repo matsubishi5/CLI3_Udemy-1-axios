4e4b393073cae453a6d08e875e22398d59cea104
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-10 16:56:53 +0900

    [learn]axios interceptors

--- src/main.js
+++ src/main.js
@@ -7,6 +7,23 @@ Vue.config.productionTip = false;
 axios.defaults.baseURL =
   'https://firestore.googleapis.com/v1/projects/vuejs-http-fe1b1/databases/(default)/documents';
 
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
