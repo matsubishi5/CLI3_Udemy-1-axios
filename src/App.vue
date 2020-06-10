ab7792264cb5f2ceef85998bf281bd8b522a43cd
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-10 14:57:03 +0900

    [learn]axios Cloud Firestore

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>掲示板に投稿する</h3>
+
+    <label for="name">ニックネーム：</label>
+    <input id="name" type="text" v-model="name" />
+    <br />
+    <br />
+    <label for="comment">コメント：</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br />
+    <br />
+
+    <button @click="createComment">コメントをサーバーに送る</button>
+
+    <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "",
+      comment: ""
+    };
+  },
+  methods: {
+    createComment() {
+      axios
+        .post(
+          "https://firestore.googleapis.com/v1/projects/vuejs-http-fe1b1/databases/(default)/documents/comments",
+          {
+            fields: {
+              name: {
+                stringValue: this.name
+              },
+              comment: {
+                stringValue: this.comment
+              }
+            }
+          }
+        )
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.name = "";
+      this.comment = "";
+    }
   }
-}
+};
 </script>
 
 <style>
